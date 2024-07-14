263d9712cdf08422834be9cfde5414602beae75c
Esat <120355601+Arastaci@users.noreply.github.com>
2024-07-14 12:12:52 +0300

    added word guessing game

---
+++ Kelime-Tahmin-Oyunu/words-db.js
@@ -0,0 +1,98 @@
+let wordList = [
+  {
+    word: "firewall",
+    hint: "ağ güvenliği duvarı",
+  },
+  {
+    word: "phishing",
+    hint: "kimlik avı saldırısı",
+  },
+  {
+    word: "malware",
+    hint: "zararlı yazılım",
+  },
+  {
+    word: "encryption",
+    hint: "veri şifreleme yöntemi",
+  },
+  {
+    word: "github",
+    hint: "kod barındırma platformu",
+  },
+  {
+    word: "vpn",
+    hint: "sanallaştırılmış özel ağ",
+  },
+  {
+    word: "antivirus",
+    hint: "zararlı yazılımlara karşı koruma",
+  },
+  {
+    word: "dos",
+    hint: "hizmet aksatma saldırısı",
+  },
+  {
+    word: "hashing",
+    hint: "veri bütünlüğü sağlama yöntemi",
+  },
+  {
+    word: "firewall",
+    hint: "ağ güvenliği cihazı",
+  },
+  {
+    word: "honeypot",
+    hint: "tuzağa düşürme sistemi",
+  },
+  {
+    word: "backdoor",
+    hint: "gizli erişim noktası",
+  },
+  {
+    word: "penetration",
+    hint: "sızma testi",
+  },
+  {
+    word: "sqlinjection",
+    hint: "veritabanı güvenlik açığı",
+  },
+  {
+    word: "xss",
+    hint: "kötü amaçlı kod enjeksiyonu",
+  },
+  {
+    word: "rootkit",
+    hint: "gizli erişim sağlama yazılımı",
+  },
+  {
+    word: "keylogger",
+    hint: "klavye girişlerini kaydeden yazılım",
+  },
+  {
+    word: "exploit",
+    hint: "zafiyetten yararlanma yöntemi",
+  },
+  {
+    word: "bugbounty",
+    hint: "ödüllü güvenlik açığı avı",
+  },
+  {
+    word: "cryptography",
+    hint: "şifreleme bilimi",
+  },
+  {
+    word: "zero-day",
+    hint: "keşfedilmemiş güvenlik açığı",
+  },
+  {
+    word: "ransomware",
+    hint: "fidye yazılımı",
+  },
+  {
+    word: "bruteforce",
+    hint: "deneme yanılma saldırısı",
+  },
+  {
+    word: "patch",
+    hint: "güvenlik güncellemesi",
+  },
+];
